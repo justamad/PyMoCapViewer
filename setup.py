67a89df3c5ea2d6aae9e05bb1a86a53fc8d36d31
JustinAlbert <justin_albert@gmx.de>
2021-11-24 18:22:49 +0100

    Changed version nr to 0.0.4. Ready to be published on PyPi and Test.PyPi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setup(
     name='PyMoCapViewer',
-    version='0.0.3',
+    version='0.0.4',
     description='A simple MoCap Viewer',
     author='Justin Albert',
     author_email='justin.albert@hpi.de',

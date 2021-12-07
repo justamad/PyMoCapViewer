d72874a7aba619bb8af56b35a6073fbf251c58b4
Justin Albert <justin.albert@hpi.de>
2021-12-07 16:27:21 +0100

    Implemented support for VNect model.

--- PyMoCapViewer/skeletons/skeleton_loader.py
+++ PyMoCapViewer/skeletons/skeleton_loader.py
@@ -1,6 +1,7 @@
 from .vicon_plug_in_gait import vicon_skeleton
 from .kinect_v2 import kinect_skeleton
 from .azure_kinect import azure_skeleton
+from .vnect import vnect_skeleton
 
 from collections import OrderedDict
 
@@ -11,6 +12,7 @@ skeleton_definitions = {
     'vicon': vicon_skeleton,
     'azure': azure_skeleton,
     'kinect_v2': kinect_skeleton,
+    'vnect': vnect_skeleton,
 }
 
 
---
+++ PyMoCapViewer/skeletons/vnect.py
@@ -0,0 +1,30 @@
+vnect_skeleton = [
+
+    # Right lower body
+    ["PELVIS", "RIGHT_HIP"],
+    ["RIGHT_HIP", "RIGHT_KNEE"],
+    ["RIGHT_KNEE", "RIGHT_ANKLE"],
+    ["RIGHT_ANKLE", "RIGHT_TOE"],
+
+    # Left lower body
+    ["PELVIS", "LEFT_HIP"],
+    ["LEFT_HIP", "LEFT_KNEE"],
+    ["LEFT_KNEE", "LEFT_ANKLE"],
+    ["LEFT_ANKLE", "LEFT_TOE"],
+
+    # Right upper body
+    ["RIGHT_WRIST", "RIGHT_ELBOW"],
+    ["RIGHT_ELBOW", "RIGHT_SHOULDER"],
+    ["RIGHT_SHOULDER", "NECK"],
+
+    # Left upper body
+    ["LEFT_WRIST", "LEFT_ELBOW"],
+    ["LEFT_ELBOW", "LEFT_SHOULDER"],
+    ["LEFT_SHOULDER", "NECK"],
+
+    # Center body
+    ["HEAD_TOP", "NOSE"],
+    ["NOSE", "NECK"],
+    ["NECK", "SPINE"],
+    ["SPINE", "PELVIS"],
+]

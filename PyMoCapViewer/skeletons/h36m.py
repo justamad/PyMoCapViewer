7dc18b951403a486fb9b4db57b959adefe78dd04
jonasnoki <Jonas.Noack@student.hpi.uni-potsdam.de>
2022-11-23 11:43:59 +0100

    Add h36m skeleton

---
+++ PyMoCapViewer/skeletons/h36m.py
@@ -0,0 +1,18 @@
+h36m_skeleton = [
+    ['root', 'torso'],
+    ['torso', 'neck'],
+    ['neck', 'nose'],
+    ['nose', 'head'],
+    ['neck', 'LShoulder'],
+    ['LShoulder', 'LElbow'],
+    ['LElbow', 'LWrist'],
+    ['neck', 'RShoulder'],
+    ['RShoulder', 'RElbow'],
+    ['RElbow', 'RWrist'],
+    ['root', 'RHip'],
+    ['RHip', 'RKnee'],
+    ['RKnee', 'RAnkle'],
+    ['root', 'LHip'],
+    ['LHip', 'LKnee'],
+    ['LKnee', 'LAnkle']
+]

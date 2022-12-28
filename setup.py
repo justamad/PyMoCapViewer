f959c2ba704d41b94e09d488a622ea50bbbe733a
Justin Albert <justin.albert@hpi.de>
2022-12-28 18:30:36 +0100

    Increase version number because of bug fix.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setup(
     name='PyMoCapViewer',
-    version='0.0.11',
+    version='0.0.12',
     description='A simple MoCap Viewer',
     author='Justin Albert',
     author_email='justin.albert@hpi.de',
